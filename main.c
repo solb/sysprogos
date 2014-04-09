54ba2ca329d623a8fb150c903f1289f09ebe7e64
Sol Boucher <slb1566@rit.edu>
2014-04-09 13:49:05 -0400

    Test 32-bit (but 64-bit wrapped) interrupt handling in compatibility mode
    We clobber all the registers in our interrupt handler, but they're
    successfully restored by the 64-bit interrupt dispatcher!

--- main.c
+++ main.c
@@ -16,6 +16,9 @@ void fake_syscall(int vector, int code) {
 	c_puts("Fake syscall called.\n");
 }
 
+void test_int_handler(int, int);
+int test_int_caller(void);
+
 int main(void) {
 	c_puts("Starting to check PTs\n");
 	for(int *spot = (int *)0x10000; spot < (int *)0x14000; ++spot) {
@@ -26,10 +29,16 @@ int main(void) {
 	}
 	c_puts("Verified all PTs\n");
 
-	c_puts("Installing isr 80\n");
+	/*c_puts("Installing isr 80\n");
 	__install_isr(80, fake_syscall);
 	c_puts("Installed ISR. Invoking\n");
-	__asm__("int $80");
+	__asm__("int $80");*/
+
+	__install_isr(80, test_int_handler);
+	if(test_int_caller())
+		c_puts("Nothing was clobbered :)\n");
+	else
+		c_puts("A dark day for Middle Earth B[\n");
 
 	c_puts("Hello, world!\n");
 	return(0);
