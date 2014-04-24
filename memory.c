b2b4cd211dc17993093c52343cfdb6d7ce5f2c7f
Sol Boucher <slb1566@rit.edu>
2014-04-23 21:44:35 -0400

    Limit memory access to superusers, with the exception of the userspace stacks, userspace binary, and some extra space following that
    We have MEMORY PROTECTION!

--- memory.c
+++ memory.c
@@ -13,18 +13,33 @@
 #include "common.h"
 
 #include "bootstrap.h"
+#include "stack.h"
 #include "startup.h"
 #include "x86arch.h"
 
 /*
 ** PRIVATE FUNCTIONS
 */
-static void __protect_system_pages(void) {
-	uint64_t *pt = (uint64_t *)PT_ADDRESS;
-	*pt &= ~PAGE_USER;
+
+/*
+** __expose_user_pages()
+**
+** allow access to pages for the userspace stack and the USERSPACE_SIZE bytes after the beginning of userspace code
+*/
+static void __expose_user_pages(void) {
+	uint64_t *pt = (uint64_t *)PT_ADDRESS + ((USERSPACE_ADDRESS - N_STACKS * sizeof(stack_t)) >> 12);
+	do {
+		*pt |= PAGE_USER;
+	}
+	while(++pt < (uint64_t *)PT_ADDRESS + ((USERSPACE_ADDRESS + USERSPACE_SIZE) >> 12));
 	__inv_tlb();
 }
 
+/*
+** __page_fault_handler(int, int)
+**
+** ISR for #PF exceptions
+*/
 static void __page_fault_handler(int vector, int code) {
 	c_printf("Page fault: code %d addr 0x%x\n", code, __get_cr2());
 	__blame_and_punish();
@@ -33,7 +48,13 @@ static void __page_fault_handler(int vector, int code) {
 /*
 ** PUBLIC FUNCTIONS
 */
+
+/*
+** _mem_init()
+**
+** Initialize the memory protection module
+*/
 void _mem_init(void) {
 	__install_isr(INT_VEC_PAGE_FAULT, __page_fault_handler);
-	__protect_system_pages();
+	__expose_user_pages();
 }
--- memory.h
+++ memory.h
@@ -4,6 +4,14 @@
 #ifndef _MEMORY_H_
 #define _MEMORY_H_
 
+// The size of the userspace memory, in bytes (excluding the stacks)
+#define USERSPACE_SIZE	0x10000
+
+/*
+** _mem_init()
+**
+** Initialize the memory protection module
+*/
 void _mem_init(void);
 
 #endif
