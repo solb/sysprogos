80d11757c2c9752bbb7bb62e9450479e2b5e304d
Christopher Cooper <cac3479@rit.edu>
2014-05-06 14:13:00 -0400

    Added implementation for reading files from the filesystem

--- filesys.c
+++ filesys.c
@@ -39,8 +39,82 @@
 */
 
 /*
+** _filesys_calc_absolute_cluster_loc - Calculates the absolute cluster location
+**										using the relative cluster number
+*/
+uint_t _filesys_calc_absolute_cluster_loc(uint_t relative_cluster)
+{
+	uint_t absolute_cluster = relative_cluster - 2 + (data_start_sector / 
+												boot_sector.sectors_per_cluster);
+	
+	return cluster_size * absolute_cluster;											
+}
+
+
+/*
+** _filesys_find_next_cluster - Gets the next relative cluster number from FAT
+**
+*/
+uint_t _filesys_find_next_cluster(uint_t current_cluster)
+{
+	return  *(uint_t *)&filesystem[fat_start_loc + current_cluster * 4];
+}
+
+/*
+** _filesys_calc_relative_cluster - Calculates the relative cluster number
+**									using the exact memory address
+*/
+uint_t _filesys_calc_relative_cluster(uint_t cluster_address)
+{
+	uint_t absolute_cluster = cluster_address / (boot_sector.bytes_per_sector * 
+													boot_sector.sectors_per_cluster);
+													
+	return absolute_cluster + 2 - (data_start_sector / boot_sector.sectors_per_cluster);
+}
+
+/*
 ** PUBLIC FUNCTIONS
 */
+
+/*
+** _filesys_readfile - finds a file at the given file address and starts reading the
+** 						file from the offset (file_address + offset) and it reads for
+**						the given number of bytes (count) and returns a byte array of 
+**						the data that was read.
+**
+*/
+void _filesys_readfile(ubyte_t *buffer, uint_t file_address, uint_t offset, uint_t count)
+{
+	uint_t relative_cluster = _filesys_calc_relative_cluster(file_address);
+	uint_t next_cluster = _filesys_find_next_cluster(relative_cluster);
+	
+	uint_t filesys_offset = file_address + offset;
+
+	uint_t remaining_bytes = count;
+	uint_t remaining_bytes_current_cluster = cluster_size - offset;
+	while(remaining_bytes > 0)
+	{
+		uint_t buffer_position = count - remaining_bytes;
+	
+		if(remaining_bytes <= remaining_bytes_current_cluster || next_cluster >= LAST_CLUSTER )
+		{ //Last cluster
+			_kmemcpy(buffer+buffer_position, filesystem+filesys_offset, remaining_bytes);
+			remaining_bytes = 0;
+			
+			continue;
+		}
+		
+		//Spans at least another cluster
+		_kmemcpy(buffer+buffer_position, filesystem+filesys_offset, cluster_size);
+		
+		//Updates remaining_bytes
+		remaining_bytes -= cluster_size;
+		
+		//Updates the file_address to next cluster location
+		filesys_offset = _filesys_calc_absolute_cluster_loc(next_cluster);
+		next_cluster = _filesys_find_next_cluster(next_cluster);
+	}
+}
 
 /*
 ** _filesys_init()
@@ -49,17 +123,50 @@
 */
 void _filesys_init( void )
 {
-	filesystem = FS_MEM_LOC;
+	//Loads the filesystem into memory
+	filesystem = FS_MEM_LOC;	
 	
-	//ZEROS out all filesystem memory
-	uint_t size = FS_SIZE_MB * 1024 * 1024; //Converts size from MB to B
-	while(size--)
-	{
-		*filesystem++ = (ubyte_t) 0;
-	}
+	//Read in filesystem boot sector information
+	boot_sector = (fat32_bs_t)
+		{ 
+			.bootjmp = {filesystem[0], filesystem[1], filesystem[2]}, 	//3 bytes
+			.oem_name = {0},											//8 bytes
+			.bytes_per_sector = *(ushort_t*)&filesystem[11], 			//2 bytes
+			.sectors_per_cluster = filesystem[13], 						//1 byte
+			.reserved_sector_count = *(ushort_t*)&filesystem[14], 		//2 bytes
+			.num_fats = filesystem[16], 								//1 byte
+			.root_entry_count = *(ushort_t*)&filesystem[17], 			//2 bytes
+			.total_sectors_16 = *(ushort_t*)&filesystem[19], 			//2 bytes
+			.media_type = filesystem[21], 								//1 byte
+			.table_size_16 = *(ushort_t*)&filesystem[22], 				//2 bytes
+			.sectors_per_track = *(ushort_t*)&filesystem[24], 			//2 bytes
+			.heads_count = *(ushort_t*)&filesystem[26], 				//2 bytes
+			.hidden_sector_count = *(uint_t*)&filesystem[28], 			//4 bytes
+			.total_sectors_32 = *(uint_t*)&filesystem[32], 				//4 bytes
+			.table_size_32 = *(uint_t*)&filesystem[36], 				//4 bytes
+			.extended_flags = *(ushort_t*)&filesystem[40], 				//2 bytes
+			.fat_version = *(ushort_t*)&filesystem[42], 				//2 bytes
+			.root_cluster = *(uint_t*)&filesystem[44], 					//4 bytes
+			.fat_info = *(ushort_t*)&filesystem[48], 					//2 bytes
+			.backup_boot_sector = *(ushort_t*)&filesystem[50], 			//2 bytes
+			.reserved = {0},				 							//12 bytes
+			.drive_number = filesystem[64], 							//1 byte
+			.reserved_1 = filesystem[65], 								//1 byte
+			.boot_signature = filesystem[66], 							//1 byte
+			.volume_id = *(uint_t*)&filesystem[67], 					//4 bytes
+			.volume_label = {0}, 										//11 bytes
+			.fat_type = {0} 											//8 bytes
+		};
 	
-	//configure fat32_bs
+	//Copies the arrays over to the boot_sector structure
+	//NEEDS TO BE CHANGED TO _kmemcpy WHEN PORTED TO OS
+	_kmemcpy(boot_sector.oem_name, filesystem+3, 8);
+	_kmemcpy(boot_sector.reserved, filesystem+52, 12);
+	_kmemcpy(boot_sector.volume_label, filesystem+71, 11);
+	_kmemcpy(boot_sector.fat_type, filesystem+82, 8);
 	
-	
-	//generate FAT with root directory
+	cluster_size = boot_sector.bytes_per_sector * boot_sector.sectors_per_cluster;
+	data_start_sector = boot_sector.reserved_sector_count + 
+									(boot_sector.num_fats * boot_sector.table_size_32);
+	fat_start_loc = boot_sector.reserved_sector_count * boot_sector.bytes_per_sector;
 }
--- filesys.h
+++ filesys.h
@@ -32,6 +32,12 @@
 */
 #define FS_MEM_LOC	 0;
 
+
+/*
+** Defines the value that represents the last cluster in a chain
+*/
+#define LAST_CLUSTER	0x0FFFFFF8
+
 /*
 ** Start of C-only definitions
 */
@@ -48,11 +54,11 @@
 typedef struct fat32_bs {
 	//Generic FAT
 	ubyte_t		bootjmp[3];
-	ubyte_t		oem_name[8];
+	char		oem_name[8];
 	ushort_t	bytes_per_sector;
 	ubyte_t		sectors_per_cluster;
 	ushort_t	reserved_sector_count;
-	ubyte_t		table_count;
+	ubyte_t		num_fats;
 	ushort_t	root_entry_count;
 	ushort_t	total_sectors_16; //For FAT32, must be 0
 	ubyte_t		media_type;
@@ -74,17 +80,17 @@ typedef struct fat32_bs {
 	ubyte_t		reserved_1;
 	ubyte_t		boot_signature;
 	uint_t		volume_id;
-	ubyte_t		volume_lable[11];
+	ubyte_t		volume_label[11];
 	ubyte_t		fat_type[8]; // For FAT32, "FAT32   "
 	
-} fat32_bs_t;
+} fat32_bs_t;		//sizeof = 96 bytes
 
 
 //File structure
 // (To make a directory: set ATTR_DIRECTORY bit in attributes
 //						 set file size to 0)
 //
-typedef struct file {
+typedef struct file_entry {
 	ubyte_t		name[11];
 	ubyte_t		attributes;
 	ubyte_t		reserved_NT; // Reserved for Windows NT. Set to 0
@@ -92,12 +98,12 @@ typedef struct file {
 	ushort_t	create_time;
 	ushort_t	create_date;
 	ushort_t	last_acces_date;
-	ushort_t	first_cluster_hi;
+	ushort_t	first_cluster_hi;	//Relateive cluster number hi
 	ushort_t	write_time;
 	ushort_t	write_date;
-	ushort_t	first_cluster_low;
+	ushort_t	first_cluster_low;	//Relative cluster number lo
 	uint_t		file_size;
-} file_t;
+} file_entry_t;		//sizeof = 32 bytes
 
 /*
 ** Globals
@@ -108,10 +114,59 @@ typedef struct file {
 */
 ubyte_t *filesystem;
 
+/*
+** Structure containing the boot_sector information read from the filesystem
+*/ 
+fat32_bs_t boot_sector;
+
+/*
+** Contains the cluster size (in bytes)
+*/
+uint_t cluster_size;
+
+/*
+**	The sector number of the start of the data section
+*/
+uint_t	data_start_sector;
+
+/*
+** FAT start sector number
+*/
+uint_t fat_start_loc;
+
 
 /*
 ** Prototypes
 */
+
+/*
+** _filesys_calc_absolute_cluster_loc - Calculates the absolute cluster location
+**										using the relative cluster number
+*/
+uint_t _filesys_calc_absolute_cluster_loc(uint_t relative_cluster);
+
+
+/*
+** _filesys_find_next_cluster - Gets the next relative cluster number from FAT
+**
+*/
+uint_t _filesys_find_next_cluster(uint_t current_cluster);
+
+/*
+** _filesys_calc_relative_cluster - Calculates the relative cluster number
+**									using the exact memory address
+*/
+uint_t _filesys_calc_relative_cluster(uint_t cluster_address);
+
+/*
+** _filesys_readfile - finds a file at the given file address and starts reading the
+** 						file from the offset (file_address + offset) and it reads for
+**						the given number of bytes.
+**
+**		The data gets read into the given buffer
+**
+*/
+void _filesys_readfile(ubyte_t *buffer, uint_t file_address, uint_t offset, uint_t amount);
 
 
 /*
