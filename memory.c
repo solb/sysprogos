313e8c74c1e87c16dd954bb15c9ecf5592c69ff5
Dan Johnson <ComputerDruid@gmail.com>
2014-05-20 13:56:33 -0400

    document the memory module

--- memory.c
+++ memory.c
@@ -16,6 +16,7 @@
 #include "stack.h"
 #include "startup.h"
 #include "x86arch.h"
+#include "scheduler.h"
 
 /*
 ** PRIVATE FUNCTIONS
@@ -26,7 +27,6 @@
 **
 ** ISR for #PF exceptions
 */
-#include "scheduler.h"
 static void __page_fault_handler(int vector, int code) {
 	c_printf("Page fault: code %d addr 0x%x rip 0x%x\n", code, __get_cr2(), _current->context->rip);
 	__blame_and_punish();
@@ -36,9 +36,6 @@ static void __page_fault_handler(int vector, int code) {
 ** PUBLIC FUNCTIONS
 */
 
-/*
-** _mem_page_frame_alloc: watermark page frame allocator
-*/
 physaddr_t _mem_page_frame_alloc(void) {
 	static uint64_t page_counter = 0;
 	void *result = DYNAMIC_MEM_LOW + (page_counter<<12);
@@ -49,16 +46,10 @@ physaddr_t _mem_page_frame_alloc(void) {
 	return (physaddr_t){result};
 }
 
-/*
-** _mem_page_frame_free: watermark page frame "deallocator"
-*/
 void _mem_page_frame_free(physaddr_t page) {
 	//I'm FREE!
 }
 
-/*
-** _mem_page_table_free: deallocate a page table and all its backing page frames
-*/
 void _mem_page_table_free(physaddr_t pt) {
 	uint64_t *entries = _mem_map_page(pt);
 	for(unsigned index = 0; index < NUM_PTES; ++index) {
@@ -69,13 +60,6 @@ void _mem_page_table_free(physaddr_t pt) {
 	_mem_unmap_page(entries);
 	_mem_page_frame_free(pt);
 }
-
-/*
-** _mem_map_page_onto: map the given page frame onto the given page-ligned memory address
-**
-** page: the page-aligned memory address onto which this frame should be mapped
-** frame: the physical address of memory the page should be mapped to.
-*/
 
 void _mem_map_page_onto(void *page, physaddr_t frame) {
 	uint64_t test = (uint64_t)page & ((1<<12)-1);
@@ -94,15 +78,6 @@ void _mem_map_page_onto(void *page, physaddr_t frame) {
 #define SCRATCH_PAGE ((void*)0x1ff000)
 static int scratch_page_mapped = 0;
 
-/*
-** _mem_map_page: map the given physical address of a page frame into virtual memory
-** returns the address where the page is mapped.
-**
-** NOTE: currently this can only map one page at a time, and will panic if a page is already
-** mapped in the scratch area. mapped memory must be unmapped with _mem_unmap_page when done
-** being used.
-*/
-
 void *_mem_map_page(physaddr_t page) {
 	if (scratch_page_mapped) {
 		_kpanic("mem", "tried to map scratch page while already mapped", FAILURE);
@@ -112,9 +87,6 @@ void *_mem_map_page(physaddr_t page) {
 	return SCRATCH_PAGE;
 }
 
-/*
-** _mem_unmap_page: unmaps the given virtual address from virtual memory
-*/
 void _mem_unmap_page(void *addr) {
 	if (addr != SCRATCH_PAGE) {
 		_kpanic("mem", "tried to unmap a non-scratch page", FAILURE);
@@ -128,27 +100,18 @@ void _mem_unmap_page(void *addr) {
 	scratch_page_mapped = 0;
 }
 
-/*
-** _mem_map_user_pagetab: map the page table of the current process at index 1
-*/
 void _mem_map_user_pagetab(physaddr_t frame) {
 	uint64_t *pdt_entry = _mem_map_page((physaddr_t){(uint64_t *)PDT_ADDRESS});
 	pdt_entry[1] = (uint64_t)frame.addr | PAGE_PRESENT | PAGE_RW | PAGE_USER;
 	_mem_unmap_page(pdt_entry);
 }
 
-/*
-** _mem_unmap_user_pagetab: unmap the first (the userspace) page table
-*/
 void _mem_unmap_user_pagetab(void) {
 	uint64_t *pdt_entry = _mem_map_page((physaddr_t){(uint64_t *)PDT_ADDRESS});
 	pdt_entry[1] = 0LL;
 	_mem_unmap_page(pdt_entry);
 }
 
-/*
-** _mem_range_is_mapped: test whether the range is within the mapped pages belonging to userland
-*/
 _Bool _mem_range_is_mapped(void *start_address, uint_t length) {
 	void *end_address = start_address + length;
 
@@ -171,21 +134,12 @@ _Bool _mem_range_is_mapped(void *start_address, uint_t length) {
 	return 1;
 }
 
-/*
-** _mem_kill_overflowing_process: terminate a process that didn't leave us
-** 			 enough room on its stack to store a context_t
-*/
 void _mem_kill_overflowing_process(void) {
 	c_printf( "Killing misbehaving process...\n\n" );
 	c_printf( "You've got to reel in your stack space requirements, man!\n" );
 	_terminate();
 }
 
-/*
-** _mem_init()
-**
-** Initialize the memory protection module
-*/
 void _mem_init(void) {
 	__install_isr(INT_VEC_PAGE_FAULT, __page_fault_handler);
 }
--- memory.h
+++ memory.h
@@ -21,15 +21,63 @@
 void _mem_init(void);
 
 
+/*
+** _mem_page_frame_alloc: watermark page frame allocator
+*/
 physaddr_t _mem_page_frame_alloc(void);
+
+/*
+** _mem_page_frame_free: watermark page frame "deallocator"
+*/
 void _mem_page_frame_free(physaddr_t page);
+
+/*
+** _mem_page_table_free: deallocate a page table and all its backing page frames
+*/
 void _mem_page_table_free(physaddr_t pt);
+
+/*
+** _mem_map_page_onto: map the given page frame onto the given page-ligned memory address
+**
+** page: the page-aligned memory address onto which this frame should be mapped
+** frame: the physical address of memory the page should be mapped to.
+*/
 void _mem_map_page_onto(void *page, physaddr_t frame);
+
+/*
+** _mem_map_page: map the given physical address of a page frame into virtual memory
+** returns the address where the page is mapped.
+**
+** NOTE: currently this can only map one page at a time, and will panic if a page is already
+** mapped in the scratch area. mapped memory must be unmapped with _mem_unmap_page when done
+** being used.
+*/
 void *_mem_map_page(physaddr_t page);
+
+/*
+** _mem_unmap_page: unmaps the given virtual address from virtual memory
+*/
 void _mem_unmap_page(void *addr);
+
+/*
+** _mem_map_user_pagetab: map the page table of the current process at index 1
+*/
 void _mem_map_user_pagetab(physaddr_t frame);
+
+/*
+** _mem_unmap_user_pagetab: unmap the first (the userspace) page table
+*/
 void _mem_unmap_user_pagetab(void);
+
+/*
+** _mem_range_is_mapped: test whether the range is within the mapped pages belonging to userland
+*/
 _Bool _mem_range_is_mapped(void *start_address, uint_t length);
+
+/*
+** _mem_kill_overflowing_process: terminate a process that didn't leave us
+** 			 enough room on its stack to store a context_t
+*/
 void _mem_kill_overflowing_process(void);
 
 #endif
